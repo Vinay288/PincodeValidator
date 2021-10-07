e8835dbb291b6138042c691be1ad0588cf827f1d
Vinay288 <vinayhiremath288@gmail.com>
2021-10-08 00:43:04 +0530

    ADDED: validate given pincode

--- PincodeValidation.js
+++ PincodeValidation.js
@@ -1,1 +1,10 @@
-const pincodeRegex = RegExp("^//d{6}$");
+const pincodeRegex = RegExp('^\\d{6}$');
+try{
+    if(pincodeRegex.test("A400088"))
+        console.log("correct")
+    else
+    throw new Error("incorrect pincode");
+}
+catch(e){
+   console.error(e)
+}
